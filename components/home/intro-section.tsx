3d4c5f7e0ce26eac714213be9a313565da9dff95
Ilmi <83828336+IrumiDesu7@users.noreply.github.com>
2023-04-30 15:36:49 +0800

    refactor(intro-section.tsx): remove unused code and simplify AvatarImage component usage

--- components/home/intro-section.tsx
+++ components/home/intro-section.tsx
@@ -1,23 +1,11 @@
-import { useAnimate } from 'framer-motion'
-import { useEffect } from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 const IntroSection = () => {
-  const [scope, animate] = useAnimate()
-  useEffect(() => {
-    // This "li" selector will only select children
-    // of the element that receives `scope`.
-    animate('p', { opacity: 0 }, { duration: 10 })
-  })
   return (
     <section id='intro' className='flex items-center justify-center p-10'>
-      <div ref={scope} className='flex flex-col items-center gap-5'>
+      <div className='flex flex-col items-center gap-5'>
         <Avatar>
-          <AvatarImage
-            src='/avatar.webp'
-            alt='irumi'
-            className='rounded-full'
-          />
+          <AvatarImage src='/avatar.webp' alt='irumi' />
           <AvatarFallback>ME</AvatarFallback>
         </Avatar>
         <p>Hi! I am Irumi.</p>
