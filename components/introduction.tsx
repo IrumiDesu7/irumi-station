d6e94a9a7b649f809f530a6a4e5c9b503aee7fa7
Ilmi <83828336+IrumiDesu7@users.noreply.github.com>
2024-08-31 02:54:37 +0800

    refactor: enhance animation timing and effects in Introduction component using GSAP

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -1,23 +1,24 @@
 'use client'
 
+import { useGSAP } from '@gsap/react'
+import { Tilt } from '@jdion/tilt-react'
 import { gsap } from 'gsap'
 import Image from 'next/image'
 import { useRef } from 'react'
-import { Tilt } from '@jdion/tilt-react'
-import { useGSAP } from '@gsap/react'
 
 export default function Introduction() {
   const introductionRef = useRef(null)
 
   useGSAP(
     () => {
-      const tl = gsap.timeline({
-        defaults: { duration: 0.5 },
-      })
-      tl.to('.wrapper', { opacity: 1 })
-      tl.from('.image', { x: -999 })
-      tl.from('.title', { x: 999 })
-      tl.from('.text', { opacity: 0, duration: 1 })
+      gsap
+        .timeline({
+          defaults: { duration: 0.5, ease: 'power2.out' },
+        })
+        .to('.wrapper', { opacity: 1 })
+        .from('.image', { x: '-100%' }, '<')
+        .from('.title', { x: '100%' }, '<')
+        .from('.text', { opacity: 0, duration: 1 }, '-=0.3')
     },
     { scope: introductionRef },
   )
@@ -27,6 +28,7 @@ export default function Introduction() {
       <div className='wrapper flex flex-col items-center gap-3 opacity-0 sm:gap-5'>
         <Tilt style={{ height: 128, width: 128 }}>
           <Image
+            priority
             src='/assets/irumi.webp'
             alt='Irumi avatar'
             width={128}
